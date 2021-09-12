123e376ee98caa3211ac2328c19620140b20ce13
e1630m <e1630m@gmail.com>
2021-09-12 13:30:19 +0000

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,6 +29,13 @@ Function gclone {
     git branch -M main
 }
 
+Function gchere {
+    $REPO = $args[0]
+    git clone git@github.com:e1630m/$REPO
+    cd $REPO
+    git branch -M main
+}
+
 Function gpub {
     $PAT = 'ghp_personal_access_tokens_of_your_GitHub_account'
     $REPO = '{"name":"' + $args[0] + '"}'
@@ -57,4 +64,11 @@ Function gpriv {
     git add README.md
     git commit -S -m "Initial Commit README.md"
     git push -u origin main
+}
+
+Function gsub {
+    $REPO = $args[0]
+    git submodule add git@github.com:e1630m/$REPO $REPO
+    cd $REPO
+    git branch -M main
 }
