4f92b059df9dfc8f607c8084325342f6f5d9079f
e1630m <e1630m@gmail.com>
2021-09-15 05:12:06 +0000

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,43 +8,43 @@
             %UserProfile%/Documents/PowerShell -> ~/.config/powershell
             %PSHOME -> /usr/local/microsoft/powershell/7
     
-    Find and Replace:
-        PAT: ghp_personal_access_tokens_of_your_GitHub_account (need repo access)
-        GitHub Username: e1630m
-        /path/to/the/home/directory/of/your/repos: ~/git
+    Replace:
+        $PAT: 'ghp_personal_access_tokens_of_your_github_account' (need repo access)
+        $GH_USERNAME: 'your_github_username'
+        $DEFAULT_GIT_PATH: '/path/to/the/home/directory/of/your/local/repos'
+
+    If this doesn't work on Windows machine(JSON parsing error), try Microsoft.PowerShell_profile_fallback.ps1
 #>
+
+$PAT = 'ghp_personal_access_tokens_of_your_GitHub_account'
+$GH_USERNAME = 'e1630m'
+$DEFAULT_GIT_PATH = '~/git'
 
 Set-PoshPrompt agnoster
 Function gcreate {
-    $PAT = 'ghp_personal_access_tokens_of_your_GitHub_account'
-    $REPO = '{"name":"' + $args[0] + '"}'
-    curl -u e1630m:$PAT https://api.github.com/user/repos -d $REPO
+    $PUB = '{"name":"' + $args[0] + '"}'
+    curl -u ${GH_USERNAME}:$PAT https://api.github.com/user/repos -d $PUB
 }
 
 Function gclone {
-    cd ~/git
+    cd $DEFAULT_GIT_PATH
     $REPO = $args[0]
-    git clone git@github.com:e1630m/$REPO
+    git clone git@github.com:$GH_USERNAME/$REPO
     cd $REPO
     git branch -M main
 }
 
 Function gchere {
     $REPO = $args[0]
-    git clone git@github.com:e1630m/$REPO
+    git clone git@github.com:$GH_USERNAME/$REPO
     cd $REPO
     git branch -M main
 }
 
 Function gpub {
-    $PAT = 'ghp_personal_access_tokens_of_your_GitHub_account'
-    $REPO = '{"name":"' + $args[0] + '"}'
-    curl -u e1630m:$PAT https://api.github.com/user/repos -d $REPO
-    cd ~/git
     $REPO = $args[0]
-    git clone git@github.com:e1630m/$REPO
-    cd $REPO
-    git branch -M main
+    gcreate $REPO
+    gclone $REPO
     echo $null >> README.md
     git add README.md
     git commit -S -m "Initial Commit README.md"
@@ -52,14 +52,10 @@ Function gpub {
 }
 
 Function gpriv {
-    $PAT = 'ghp_personal_access_tokens_of_your_GitHub_account'
-    $REPO = '{"name":"' + $args[0] + '", "private":"true"}'
-    curl -u e1630m:$PAT https://api.github.com/user/repos -d $REPO
-    cd ~/git
     $REPO = $args[0]
-    git clone git@github.com:e1630m/$REPO
-    cd $REPO
-    git branch -M main
+    $PRIV = '{"name":"' + $args[0] + '", "private":"true"}'
+    curl -u ${GH_USERNAME}:$PAT https://api.github.com/user/repos -d $PRIV
+    gclone $REPO
     echo $null >> README.md
     git add README.md
     git commit -S -m "Initial Commit README.md"
@@ -68,7 +64,7 @@ Function gpriv {
 
 Function gsub {
     $REPO = $args[0]
-    git submodule add git@github.com:e1630m/$REPO $REPO
+    git submodule add git@github.com:${GH_USERNAME}/$REPO $REPO
     cd $REPO
     git branch -M main
 }
